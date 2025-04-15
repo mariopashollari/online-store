ef09aa63738181ee62f9e03b811dba88c5fcf1cd
mariopashollari <mariopashollari9@gmail.com>
2025-04-16 00:02:42 +0200

    finished Security. Also added a maven dependency because it wouldnt read WebMvcConfigurer for some reason even though it should be inside spring security?

---
+++ src/main/java/com/individual/online_store/security/UserDetailServiceImpl.java
@@ -0,0 +1,24 @@
+package com.individual.online_store.security;
+
+
+import com.individual.online_store.entities.User;
+import com.individual.online_store.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserDetailServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username).orElseThrow();
+        return new UserDetailsImpl(user);
+    }
+}
---
+++ src/main/java/com/individual/online_store/security/UserDetailsImpl.java
@@ -0,0 +1,60 @@
+package com.individual.online_store.security;
+
+
+import com.individual.online_store.entities.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private String username;
+    private String password;
+    private Boolean active;
+    private List<GrantedAuthority> authorities;
+
+    public UserDetailsImpl(User user) {
+        this.username = user.getEmail();
+        this.password = user.getPassword();
+        this.active = user.getActive();
+        this.authorities = List.of(new SimpleGrantedAuthority(user.getRole().getName()));
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return active;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return active;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return active;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return active;
+    }
+}
